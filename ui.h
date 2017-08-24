fe9d536f4a9ab8e74e379a30bcd1c8e37e4e7cde
Tom Marshall <tdm.code@gmail.com>
2017-08-24 13:50:01 +0000

    recovery: ui: Support hardware virtual keys
     * Also swipe left -> KEY_BACK
    
    Change-Id: I6bd8054485d680df35abb86cb79f1dda683e4459

--- ui.h
+++ ui.h
@@ -23,8 +23,6 @@
 
 #include <string>
 
-#define MAX_NR_VKEYS	8
-
 /*
  * Simple representation of a (x,y) coordinate with convenience operators
  */
@@ -211,6 +209,7 @@ class RecoveryUI {
   const int kTouchLowThreshold;
   const int kTouchHighThreshold;
 
+  void OnTouchDeviceDetected(int fd);
   void OnKeyDetected(int key_code);
   void OnTouchEvent();
   int OnInputEvent(int fd, uint32_t epevents);
@@ -242,12 +241,19 @@ class RecoveryUI {
   bool has_down_key;
   bool has_touch_screen;
 
+  struct vkey_t {
+    int keycode;
+    Point min_;
+    Point max_;
+  };
+
   // Touch event related variables. See the comments in RecoveryUI::OnInputEvent().
   int touch_slot_;
   Point touch_pos_;
   Point touch_start_;
   bool touch_finger_down_;
   bool touch_swiping_;
+  std::vector<vkey_t> virtual_keys_;
   bool is_bootreason_recovery_ui_;
 
   pthread_t input_thread_;
