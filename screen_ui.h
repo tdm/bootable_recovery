LineageOS/android_bootable_recovery

recovery: Fix redraws, flickering, and animation
Rewrite screen drawing code to behave as follows:

 * draw_screen_locked has exactly two code paths: menu and non-menu.
    Non-menu code path draws background, foreground, and text.
 * Define screen background as the stage marker.
 * Define screen foreground as either the animation and progress bar
   or the icon and text.
 * Restore redraw optimizations in update_progress_locked.
 * Get rid of spurious screen updates.  The screen is only updated
    in StartMenu, SelectMenu, Redraw, and the progress thread.
 * Various changes and fixes to support the above.

Additionally:

 * Show error text on screen after failed install.
 * Rewrite graphics test code to better exercise the above.

Change-Id: I598bb11d5eef28275a6501cd44d514afe9939885

screen_ui.h
@@ -101,7 +101,7 @@ class ScreenRecoveryUI : public RecoveryUI {
 
   void KeyLongPress(int) override;
 
-  void Redraw();
+  void Redraw() override;
 
   enum UIElement {
     STATUSBAR,
@@ -205,7 +205,7 @@ class ScreenRecoveryUI : public RecoveryUI {
   virtual bool InitTextParams();
 
   virtual void draw_background_locked();
-  virtual void draw_foreground_locked();
+  virtual void draw_foreground_locked(int& y);
   virtual void draw_statusbar_locked();
   virtual void draw_header_locked(int& y);
   virtual void draw_text_menu_locked(int& y);

tdm/bootable_recovery

screen_ui.h
@@ -113,7 +113,7 @@ class ScreenRecoveryUI : public RecoveryUI {
 
   void KeyLongPress(int) override;
 
-  void Redraw();
+  void Redraw() override;
 
   void SetColor(UIElement e) const;
 
@@ -135,7 +135,7 @@ class ScreenRecoveryUI : public RecoveryUI {
 
   virtual bool InitTextParams();
 
-  virtual void draw_background_locked();
+  virtual void draw_background_locked(int& y);
   virtual void draw_foreground_locked();
   virtual void draw_statusbar_locked();
   virtual void draw_header_locked(int& y);